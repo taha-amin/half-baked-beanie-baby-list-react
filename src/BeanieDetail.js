5f1d6595faccefeaf0a5fe26ef654a65fa5e09cd
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-05-09 14:04:09 -0700

    wrote state for keeping track the current beanieBaby and getting the route param using the appropriate react router hook

--- src/BeanieDetail.js
+++ src/BeanieDetail.js
@@ -1,15 +1,18 @@
 import { useEffect, useState } from 'react';
+import { useRouteMatch } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { getSingleBeanie } from './services/fetch-utils';
 
 export default function BeanieDetail() {
   // you'll need to track the current beanieBaby in state
+  const [beanieBaby, setBeanieBaby] = useState([]);
+
   // you'll need to get the route param using the appropriate react router hook
+  const match = useRouteMatch();
 
   useEffect(() => {
     // you'll need to define a fetch function here (then call it below) that gets this page's beanie baby and injects it into state using the correct state handler
-
-  }, []); // note that you'll want the id from the url in the dependency array because you want the useEffect callback to get called every time the url changes 
+  }, []); // note that you'll want the id from the url in the dependency array because you want the useEffect callback to get called every time the url changes
 
   function handleBeanieClick() {
     // here's a challenge. How can you link on click to the beanie baby's correct entry in the official beanie baby fan site?
@@ -18,7 +21,7 @@ export default function BeanieDetail() {
   return (
     <>
       {/* Feel free to uncomment and use the below code--but you'll need to figure out how to keep it from blowing up on load */}
-      
+
       {/* <Link to='/'>Home</Link>
       <div className='beanie-detail' onClick={handleBeanieClick}>
         <div className='beanie-data'>
