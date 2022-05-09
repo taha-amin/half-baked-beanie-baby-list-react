c03b7b732b1a8b1ae3ee33cc217738cdb86a1d7c
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-05-09 14:30:58 -0700

    in app.js wrote exact paths. Listed all the beanies on the home page route and pointed to a particular beanie baby by id and rendered that specific beanieDetail page

--- src/App.js
+++ src/App.js
@@ -8,9 +8,13 @@ export default function App() {
     <Router>
       <div className="App">
         <Switch>
-          <Route>{/* this home page route should list all the beanies */}</Route>
-          <Route>
+          <Route exact path="/">
+            {/* this home page route should list all the beanies */}
+            <BeaniesPage />
+          </Route>
+          <Route exact path="/beanies/:id">
             {/* this route should point to a particular beanie baby by id and render that specific BeanieDetail page */}
+            <BeanieDetail />
           </Route>
         </Switch>
       </div>
