4dbbfacf7e07be25faf88a7bb30de5f0059142f5
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-05-09 12:33:27 -0700

    rendered out the beanie babies as a list in beanieslist.js

--- src/BeaniesList.js
+++ src/BeaniesList.js
@@ -2,7 +2,11 @@ import BeanieBaby from './BeanieBaby.js';
 
 export default function BeaniesList({ beanieBabies }) {
   return (
-    <div className='beanies'>
+    <div className="beanies">
       {/* render out the beanie babies as a list */}
-    </div>);
+      {beanieBabies.map((beanieBaby) => (
+        <BeanieBaby key={`${beanieBaby.id}${beanieBaby.title}`} beanieBaby={beanieBaby} />
+      ))}
+    </div>
+  );
 }
