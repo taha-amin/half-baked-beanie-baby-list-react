d5c273615379f5dbde843377153b2cb8025a1206
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-05-09 12:38:13 -0700

    contained a react-router-dom Link to the detail page for the particular beanie baby. Also rendered the beanie baby's image and shows the beanie baby's name

--- src/BeanieBaby.js
+++ src/BeanieBaby.js
@@ -4,6 +4,11 @@ export default function BeanieBaby({ beanieBaby }) {
   return (
     // this should contain a react-router-dom Link to the detail page for this particular beanie baby.
     // it should also render the beanie baby's image and show the beanie baby's name
-    null
+    <Link to={`/beanies/${beanieBaby.id}`}>
+      <div className="beanie-baby">
+        <img className="beanie-img" src={beanieBaby.image} />
+        <p>{beanieBaby.title}</p>
+      </div>
+    </Link>
   );
 }
