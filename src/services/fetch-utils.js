c33b3d845c6069da0f41a739f56c5b57abe08eb6
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-05-09 17:01:29 -0700

    refactored code

--- src/services/fetch-utils.js
+++ src/services/fetch-utils.js
@@ -1,21 +1,13 @@
 import { client, checkError } from './client';
 
 export async function getBeanieBabies(from = 0, to = 30) {
-  const response = await client
-    .from('beanie_babies')
-    .select()
-    .range(from, to);
+  const response = await client.from('beanie_babies').select().range(from, to);
 
   return checkError(response);
 }
 
 export async function getSingleBeanie(id) {
-  const response = await client
-    .from('beanie_babies')
-    .select()
-    .match({ id })
-    .single();
+  const response = await client.from('beanie_babies').select().match({ id }).single();
 
   return checkError(response);
-
 }
